d02afefd1cc908c5d66db1a4241f26b0369dd22a
kylemeier <kyledmeier@gmail.com>
2013-09-30 19:32:57 -0700

    re-wrote dynamic scroll
    now uses animate function, cleaner and bug free

--- Carousel/carouseljs.js
+++ Carousel/carouseljs.js
@@ -1,32 +1,17 @@
-function pageScroll(endLocation){
-    if (endLocation == 'home'){
-        anchor = $('#firststop').offset().top - 50;
-    } else if (endLocation == 'about'){
-        anchor = $('#secondstop').offset().top - 50;
-    }else if (endLocation == 'contact'){
-        anchor = $('#thirdstop').offset().top - 50;
-    }else{
-        return;
-    }
-	distanceTraveled = 0;
-	currentLocation = $(window).scrollTop();
-	travelDistance = anchor - currentLocation;
-	travelSpeed = Math.round(travelDistance/50);
-	scrolldelay = setInterval('scroll()',10);	
-}
+var home = $('#firststop').offset().top - 50;
+var about = $('#secondstop').offset().top - 50;
+var contact = $('#thirdstop').offset().top - 50;
 
-function scroll() {
-		if (distanceTraveled<Math.abs(travelDistance-travelSpeed)){
-    	window.scrollBy(0,travelSpeed); // horizontal and vertical scroll increments
-    	distanceTraveled += Math.abs(travelSpeed);
-    	//alert(distanceTraveled);
-    	}else{
-    		distanceTraveled=0;
-    		window.scrollTo(0,anchor);
-    		clearTimeout(scrolldelay);
-    	}
-}
-function stopScroll() {
-    	clearTimeout(scrolldelay);
-}
+$('#home').click(function(){
+            $('html,body').animate({scrollTop:home},'slow');
+            return false;
+        });
 
+$('#about').click(function(){
+            $('html,body').animate({scrollTop:about},'slow');
+            return false;
+        });
+$('#contact').click(function(){
+            $('html,body').animate({scrollTop:contact},'slow');
+            return false;
+        });
